a10fb0610242a111890d3d5f6148d6308c50d691
anusoda <35657704+anusoda@users.noreply.github.com>
2022-05-27 08:42:18 -0400

    http/https fix

--- Maintenance/src/SongCreateBox.java
+++ Maintenance/src/SongCreateBox.java
@@ -145,8 +145,7 @@ public class SongCreateBox implements ActionListener, KeyListener
 			{
 				box1.setText(" ");
 			}
-			if(!box2.getText().contains("http://"))
-			{
+			if(!(box2.getText().contains("http://") || box2.getText().contains("https://"))) {
 				box2.setText("http://" + box2.getText());
 			}
 			data.add(box1.getText());
