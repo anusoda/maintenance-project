02c35ac5702bf8af505ee31758e313c3c14e52db
ksun9435 <87787907+ksun9435@users.noreply.github.com>
2022-06-07 08:52:12 -0400

    Update PlayHandler.java

--- Maintenance/src/PlayHandler.java
+++ Maintenance/src/PlayHandler.java
@@ -6,8 +6,13 @@ import java.util.ArrayList;
 
 public class PlayHandler {
 	private static String OS;
+	private Header header;
 	public PlayHandler() {
 		OS = System.getProperty("os.name").toLowerCase();
+	}
+
+	public void setHeader(Header frame) {
+		header=frame;
 	}
 	public static enum Mode
 	{
@@ -113,6 +118,10 @@ public class PlayHandler {
 
 	public void switchToShuffle() {
 		mode = Mode.Shuffle;
+		header.changeMode(0);
+		header.setDefaultCloseOperation(header.EXIT_ON_CLOSE);
+		header.pack();
+		header.setVisible(true);
 		playNext();
 	}
 
